da502a3d14d30c119ffac56da06e95a97de834ec
joseacruz428-star <joseacruz.428@gmail.com>
2026-03-02 00:20:00 -0300

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const Workout = require('../models/Workout')
+const Workout = require('../models/workout')
 const protect = require('../middleware/auth')
 
 const role = require('../middleware/role')
